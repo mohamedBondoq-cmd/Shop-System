0dcab066a88d2bc8590ca44c9075774869c294a5
Mohamed <moham@LAPTOP-51ASB954>
2026-06-30 01:24:49 +0300

    Add Data To DataBase

---
+++ Shop System/Migrations/20260629213703_AddDataToCategories.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataToCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into categories (Name, Description, Status) values ('Mobiles', 'Sed sagittis.', 1);insert into categories (Name, Description, Status) values ('Laptops', 'Pellentesque viverra pede ac diam.', 0);insert into categories (Name, Description, Status) values ('Accessoris', 'Nulla mollis molestie lorem.', 1);insert into categories (Name, Description, Status) values ('Cameras', 'Duis aliquam convallis nunc.', 1);insert into categories (Name, Description, Status) values ('Tablets', 'Proin at turpis a pede posuere nonummy.', 1);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM categories");
+        }
+    }
+}
---
+++ Shop System/Migrations/20260629220623_AddDataToBrands.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataToBrands : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into Brands (Name, Description, Status) values ('Apple', 'In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.', 0);insert into Brands (Name, Description, Status) values ('Samsung', 'Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla.', 0);insert into Brands (Name, Description, Status) values ('Lg', 'Proin risus. Praesent lectus.', 0);insert into Brands (Name, Description, Status) values ('Oppo', 'Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', 1);insert into Brands (Name, Description, Status) values ('Dell', 'Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo. Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante. Vivamus tortor.', 1);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Brands");
+        }
+    }
+}
