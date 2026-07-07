1659238600e1d5b2f524ced7acdec71d9ad41e3f
mohamed.shaban <mohamed.shaban@ZAYARD-W2>
2026-07-07 17:47:29 +0300

    Add Information To Products

--- Shop System/Controllers/HomeController.cs
+++ Shop System/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Shop_System.DataAccess;
 using Shop_System.Models;
 using System.Diagnostics;
+
 
 namespace Shop_System.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _context = new ApplicationDbContext();
         public IActionResult Index()
         {
-            return View();
+            var Products = _context.products.AsQueryable();
+            return View(Products.ToList());
         }
 
         public IActionResult Privacy()
